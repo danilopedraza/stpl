559bf36bed49627ef8b2a1e804d99b4725138fb2
César Pedraza <cpedraza@unal.edu.co>
2023-07-15 14:01:09 -0500

    more tests and a big refactor

--- lib/lexer.dart
+++ lib/lexer.dart
@@ -1,17 +1,45 @@
 enum Token {
-    eof(),
-    colon();
+    eof(''),
+    colon(':'),
+    lineBreak('\n');
+
+    final String _exp;
+    const Token(this._exp);
+
+    String get exp => '^$_exp\$';
+
+    bool matches(String str) => RegExp(exp).hasMatch(str);
 }
 
 class Lexer {
     final String _input;
+    int _lookaheadIndex = 0;
     
     Lexer(this._input);
 
+    void consume() {
+        _lookaheadIndex++;
+    }
+
     Token nextToken() {
-        if (_input == '') {
+        if (_lookaheadIndex >= _input.length) {
             return Token.eof;
         }
-        return Token.colon;
+
+        while (_lookaheadIndex < _input.length && (_input[_lookaheadIndex] == ' ' || _input[_lookaheadIndex] == '\t' || _input[_lookaheadIndex] == '\r')) {
+            consume();
+        }
+
+        final int start = _lookaheadIndex + 0;
+
+        while (_lookaheadIndex < _input.length && (_input[_lookaheadIndex] != ' ' && _input[_lookaheadIndex] != '\t' && _input[_lookaheadIndex] != '\r')) {
+            consume();
+        }
+
+        final int end = _lookaheadIndex + 0;
+
+        final String substring = _input.substring(start, end);
+
+        return Token.values.firstWhere((token) => token.matches(substring));
     }
 }
--- test/lexer_test.dart
+++ test/lexer_test.dart
@@ -3,12 +3,26 @@ import 'package:test/test.dart';
 
 void main() {
   test('The next token from a lexer with an empty string should be of type eof', () {
-    
     expect(Lexer('').nextToken(), Token.eof);
   });
 
+  test('The next token from a lexer with a space should be of type eof', () {
+    expect(Lexer(' ').nextToken(), Token.eof);
+  });
+
+  test('The next token from a lexer with two spaces should be of type eof', () {
+    expect(Lexer('  ').nextToken(), Token.eof);
+  });
+
+  test('The next token from a lexer with separators only should be of type eof', () {
+    expect(Lexer(' \t\r').nextToken(), Token.eof);
+  });
+
   test('The next token from a lexer with a string with just a colon should be of type colon', () {
-    
     expect(Lexer(':').nextToken(), Token.colon);
+  });
+
+  test('The next token from a lexer with a line break should be of type lineBreak', () {
+    expect(Lexer('\n').nextToken(), Token.lineBreak);
   });
 }
