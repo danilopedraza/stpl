92d27c30a717057760a3d46622b375571dd97e66
César Pedraza <cpedraza@unal.edu.co>
2023-08-09 11:30:19 -0500

    add markdown table formatting
    Very frail...

--- lib/formatter.dart
+++ lib/formatter.dart
@@ -12,4 +12,27 @@ class Formatter {
 
   String get csv =>
       [rowLabels, ...table].map((row) => row.join(",")).join("\n");
+
+  List<int> get columnLengths => [8, 4, 4, 4];
+
+  String markdownRow(List<String> row) {
+    String res = '|';
+
+    for (int i = 0; i < 4; i++) {
+      var filler = columnLengths[i] - row[i].length;
+
+      res += ' ${row[i]}${' ' * filler} |';
+    }
+
+    return res;
+  }
+
+  String get markdown {
+    List<String> rows = [
+      '| Exercise | sets | reps | load |',
+      '| -------- | ---- | ---- | ---- |',
+    ];
+
+    return rows.followedBy(table.map(markdownRow)).join('\n');
+  }
 }
--- test/formatter_test.dart
+++ test/formatter_test.dart
@@ -80,4 +80,52 @@ void main() {
     expect(Formatter(evaluator.nextSession()).csv,
         equals('Exercise,sets,reps,load\nsquat,3,5,60kg\npress,3,10,21kg'));
   });
+
+  test(
+      'the formatter should transform a prescription into a Markdown-style row with correct column names',
+      () {
+    const String input = '''SESSION a:
+                            squat 3x5
+
+                            PROGRESSION:
+                            squat goes up 5kg eVeRy TiMe
+
+                            TRAINING SESSION 1 (a):
+                            squat 3x5x55kg
+''';
+    Evaluator evaluator = Evaluator(Parser(Lexer(input)));
+    expect(
+        Formatter(evaluator.nextSession()).markdown,
+        equals([
+          '| Exercise | sets | reps | load |',
+          '| -------- | ---- | ---- | ---- |',
+          '| squat    | 3    | 5    | 60kg |',
+        ].join('\n')));
+  });
+
+  test(
+      'the formatter should transform several prescriptions into a Markdown-style row with correct column names',
+      () {
+    const String input = '''SESSION a:
+                            squat 3x5
+                            press 3x10
+
+                            PROGRESSION:
+                            squat goes up 5kg EVERY TIME
+                            press goes up 1kg EVERY TIME
+
+                            TRAINING SESSION 1 (a):
+                            squat 3x5x55kg
+                            press 3x10x20kg
+''';
+    Evaluator evaluator = Evaluator(Parser(Lexer(input)));
+    expect(
+        Formatter(evaluator.nextSession()).markdown,
+        equals([
+          '| Exercise | sets | reps | load |',
+          '| -------- | ---- | ---- | ---- |',
+          '| squat    | 3    | 5    | 60kg |',
+          '| press    | 3    | 10   | 21kg |',
+        ].join('\n')));
+  });
 }
