b58aacb143c8ec7d396832e02af7f7225875f9c1
César Pedraza <cpedraza@unal.edu.co>
2023-08-14 21:21:38 -0500

    implemented two error cases for the CLI manager

--- lib/cli.dart
+++ lib/cli.dart
@@ -2,6 +2,11 @@ class CLIManager {
   final List<String> arguments;
   CLIManager(this.arguments);
 
-  String get response =>
-      'A command-line utility for STPL.\nUsage: stpl <file> <command>';
+  String get response {
+    if (arguments.length < 2) {
+      return 'A command-line utility for STPL.\nUsage: stpl <file> <command>';
+    }
+
+    return 'Could not find a command named "${arguments[1]}"';
+  }
 }
--- test/cli_test.dart
+++ test/cli_test.dart
@@ -9,4 +9,20 @@ void main() {
         'A command-line utility for STPL.\nUsage: stpl <file> <command>';
     expect(CLIManager([]).response, equals(expected));
   });
+
+  test(
+      'The CLI manager should give a specific help message when an unknown command is passed',
+      () {
+    String command = 'foo';
+    String expected = 'Could not find a command named "$command"';
+    expect(CLIManager(['program.stpl', command]).response, equals(expected));
+  });
+
+  test(
+      'The CLI manager should give the generic help message when a file is passed but not a command',
+      () {
+    String expected =
+        'A command-line utility for STPL.\nUsage: stpl <file> <command>';
+    expect(CLIManager(['program.stpl']).response, equals(expected));
+  });
 }
