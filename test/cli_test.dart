f5afdfa72f6385149e89da42cf5faeeafb28a337
César Pedraza <cpedraza@unal.edu.co>
2023-08-14 21:23:17 -0500

    changed declarations to final

--- test/cli_test.dart
+++ test/cli_test.dart
@@ -5,7 +5,7 @@ void main() {
   test(
       'The CLI manager should give a help message when there are no CLI arguments',
       () {
-    String expected =
+    final String expected =
         'A command-line utility for STPL.\nUsage: stpl <file> <command>';
     expect(CLIManager([]).response, equals(expected));
   });
@@ -13,15 +13,15 @@ void main() {
   test(
       'The CLI manager should give a specific help message when an unknown command is passed',
       () {
-    String command = 'foo';
-    String expected = 'Could not find a command named "$command"';
+    final String command = 'foo';
+    final String expected = 'Could not find a command named "$command"';
     expect(CLIManager(['program.stpl', command]).response, equals(expected));
   });
 
   test(
       'The CLI manager should give the generic help message when a file is passed but not a command',
       () {
-    String expected =
+    final String expected =
         'A command-line utility for STPL.\nUsage: stpl <file> <command>';
     expect(CLIManager(['program.stpl']).response, equals(expected));
   });
