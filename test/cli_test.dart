ab1aa41ab7c33fa8ad0d23a5888e58bf8f588071
César Pedraza <cpedraza@unal.edu.co>
2023-08-16 20:38:59 -0500

    change test order

--- test/cli_test.dart
+++ test/cli_test.dart
@@ -12,19 +12,19 @@ void main() {
   });
 
   test(
+      'The CLI manager should give the generic help message when a file is passed but not a command',
+      () {
+    final String expected =
+        'A command-line utility for STPL.\nUsage: stpl <file> <command>';
+    expect(CLIManager.fromArgs(['program.stpl']).response, equals(expected));
+  });
+
+  test(
       'The CLI manager should give a specific help message when an unknown command is passed',
       () {
     final String command = 'foo';
     final String expected = 'Could not find a command named "$command"';
     expect(CLIManager(['program.stpl', command], File('program.stpl')).response,
         equals(expected));
-  });
-
-  test(
-      'The CLI manager should give the generic help message when a file is passed but not a command',
-      () {
-    final String expected =
-        'A command-line utility for STPL.\nUsage: stpl <file> <command>';
-    expect(CLIManager.fromArgs(['program.stpl']).response, equals(expected));
   });
 }
